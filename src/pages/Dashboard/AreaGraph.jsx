0d7f2ca8260bc173f84b9e664259ff899ae52383
DiegoCuba <diego.netsolutions@gmail.com>
2020-10-09 04:19:02 -0400

    responsive almost OK

--- src/pages/Dashboard/AreaGraph.jsx
+++ src/pages/Dashboard/AreaGraph.jsx
@@ -1,16 +1,16 @@
 import React, { PureComponent } from 'react';
 import {
-    LineChart, Line, XAxis, YAxis, Tooltip, Brush, AreaChart, Area, ResponsiveContainer
+    LineChart, Line, XAxis, YAxis, Tooltip, Brush, AreaChart, Area
 } from 'recharts';
 
 
 function getViewPortWidth() {
     var viewportwidth;
-    // the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
-    if (typeof window.innerWidth != 'undefined') {
+    // the more standards compliant browsers (mozilla/netscape/opera/IE7) 
+    if (typeof window.innerWidth !== 'undefined') {
         viewportwidth = window.innerWidth;
         // IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
-    } else if (typeof document.documentElement != 'undefined' && typeof document.documentElement.clientWidth != 'undefined' && document.documentElement.clientWidth != 0) {
+    } else if (typeof document.documentElement !== 'undefined' && typeof document.documentElement.clientWidth !== 'undefined' && document.documentElement.clientWidth !== 0) {
         viewportwidth = document.documentElement.clientWidth;
         // older versions of IE
     } else {
@@ -23,30 +23,29 @@ export default class AreaGraph extends PureComponent {
 
     render() {
         return (
-            <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-                <div style={{ width: '100%', height: 300 }}>
-                    <ResponsiveContainer >
-                        <AreaChart
-                            width={Math.floor(getViewPortWidth() * 80)}
-                            height={200}
-                            data={this.props.data}
-                            syncId="anyId"
-                            margin={{
-                                top: 10, right: 30, left: 0, bottom: 0,
-                            }}
-                        >
+            <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
 
-                            <XAxis dataKey="name" />
-                            <YAxis />
-                            <Tooltip />
-                            <Area type="monotone" dataKey="Ganancia Neta" stroke="#82ca9d" fill="#82ca9d" />
-                            <Area type="monotone" dataKey="Inversiones" stroke="#8884d8" fill="#8884d8" />
-                            <Brush />
-                        </AreaChart>
-                    </ResponsiveContainer></div>
+                <AreaChart
+                    width={Math.floor(getViewPortWidth() * 0.8)}
+                    height={200}
+                    data={this.props.data}
+                    syncId="anyId"
+                    margin={{
+                        top: 10, right: 30, left: 0, bottom: 0,
+                    }}
+                >
+
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Area type="monotone" dataKey="Ganancia Neta" stroke="#82ca9d" fill="#82ca9d" />
+                    <Area type="monotone" dataKey="Inversiones" stroke="#8884d8" fill="#8884d8" />
+                    <Brush />
+                </AreaChart>
+
 
                 <LineChart
-                    width={400}
+                    width={getViewPortWidth() > 800 ? Math.floor(getViewPortWidth() * 0.4) : Math.floor(getViewPortWidth() * 0.8)}
                     height={200}
                     data={this.props.data}
                     syncId="anyId"
@@ -62,7 +61,7 @@ export default class AreaGraph extends PureComponent {
                 </LineChart>
 
                 <LineChart
-                    width={400}
+                    width={getViewPortWidth() > 800 ? Math.floor(getViewPortWidth() * 0.4) : Math.floor(getViewPortWidth() * 0.8)}
                     height={200}
                     data={this.props.data}
                     syncId="anyId"
